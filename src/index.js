ec13d0a61ab2491f1c683c0ff746da5b82bbf61a
Edgar Ong <emailedgar@gmail.com>
2019-05-15 11:13:50 -0400

    start bonus

--- src/index.js
+++ src/index.js
@@ -27,4 +27,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   })
 
+  // BONUS
+  // - A delete function that will remove tasks from your list
+  // - A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
+  //   - As a challenge, implement a sorting functionality that displays the tasks ASC/DESC based on priority
+  // - An additional input field (e.g. user, duration, date due)
+  // - Ability to edit tasks
+  // - Something of your choice! The main objective is to add a feature that allows the user's input to affect the DOM
 });
