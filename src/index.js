cc5e3f0aa24e7396003c4c58c277c6d0734aa831
Edgar Ong <emailedgar@gmail.com>
2019-05-15 11:47:28 -0400

    add delete to task

--- src/index.js
+++ src/index.js
@@ -15,24 +15,37 @@ document.addEventListener("DOMContentLoaded", () => {
     // show tasks
     const taskShow = document.querySelector("#tasks")
     // console.log('task: ', taskShow)
+
     // create new list items
     const newLi = document.createElement('li')
     // console.log('new li: ', newLi)
+
     // add list item values
     const newTask = input.value
-    console.log('current input value: ', newTask)
+    // console.log('current input value: ', newTask)
 
     taskShow.append(newLi)
 
     // setting a key id for each task item
-    let i = Math.ceil(Math.random()*1000)
-    newLi.id = newTask.slice(0,10) + '-' + i
+    // let i = Math.ceil(Math.random()*1000, 6)
+    // newLi.id = newTask.slice(0,6) + '-' + i
 
     newLi.innerText = newTask
 
+    console.log('task item: ', newLi)
+
   // BONUS
   // - A delete function that will remove tasks from your list
+    const newButton = document.createElement("button")
+    newButton.id = "delete"
+    newButton.innerText = "x"
+    console.log("delete button: ", newButton)
+    newLi.appendChild(newButton)
 
+    newButton.addEventListener("click", function(){
+      newLi.remove()
+      console.log("Deleting: ", newLi.innerText)
+    })
   // - A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
   //   - As a challenge, implement a sorting functionality that displays the tasks ASC/DESC based on priority
   // - An additional input field (e.g. user, duration, date due)
